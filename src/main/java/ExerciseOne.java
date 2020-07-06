164426c91c598e3e2d348fc4e43f4faaa08d7c03
Leyla <leyla.hunn@outlook.com>
2020-07-06 17:00:23 +0100

    initial commit

--- src/main/java/ExerciseOne.java
+++ src/main/java/ExerciseOne.java
@@ -1,6 +1,24 @@
-public class ExerciseOne {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
-    public static void main(String args[]){
+public class ExerciseOne {
+    /* Go to our test site, the-internet, and use the findElement and findElements to complete the following scenario.
+
+Click on the “Shifting Content” link.
+
+     */
+
+    public void setUp(){
 
     }
+
+
+    public static void main(String args[]){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+    }
+
+
 }
